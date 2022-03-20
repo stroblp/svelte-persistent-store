0fa0b8b85d9efa9fc3bd5b3e79ac40f6952de178
stroblp <stroblp@hotmail.com>
2022-03-20 20:57:46 +0000

    update example

--- src/App.svelte
+++ src/App.svelte
@@ -9,13 +9,25 @@
 <button on:click={() => ($countlocalStorage -= 1)}>-</button>
 <span>{$countlocalStorage}</span>
 <button on:click={() => ($countlocalStorage += 1)}>+</button>
+<button on:click={() => ($countlocalStorage =0)}>Clear</button>
 
 <p>websql:</p>
 <button on:click={() => ($countWebSQL -= 1)}>-</button>
 <span>{$countWebSQL}</span>
 <button on:click={() => ($countWebSQL += 1)}>+</button>
+<button on:click={() => ($countWebSQL =0)}>Clear</button>
 
 <p>indexeddb:</p>
 <button on:click={() => ($countIndexedDB -= 1)}>-</button>
 <span>{$countIndexedDB}</span>
 <button on:click={() => ($countIndexedDB += 1)}>+</button>
+<button on:click={() => ($countIndexedDB =0)}>Clear</button>
+
+<div>
+	<button on:click={() => (location.reload())}>Reload Page</button>
+</div>
+<style>
+	div{
+		padding:10px 0px;
+	}
+</style>
