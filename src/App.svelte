0d8dd00bb6ef5a3ca92fd6317167da2cd2f073c7
stroblp <stroblp@hotmail.com>
2022-03-20 17:42:57 +0000

    add examples

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,21 @@
 <script>
-	import { writableIndexedDB } from './presistent-store';
-	let count = writableIndexedDB('counter-value',0);
+	import { presistentStore } from "./presistent-store";
+	let countlocalStorage = presistentStore("counter-value", 0, "LOCALSTORAGE");
+	let countWebSQL = presistentStore("counter-value", 0,"WEBSQL");
+	let countIndexedDB = presistentStore("counter-value", 0);
 </script>
 
-<button on:click={() => $count -= 1}>-</button>
-<span>{ $count }</span>
-<button on:click={() => $count += 1}>+</button>
+<p>localstorage:</p>
+<button on:click={() => ($countlocalStorage -= 1)}>-</button>
+<span>{$countlocalStorage}</span>
+<button on:click={() => ($countlocalStorage += 1)}>+</button>
+
+<p>websql:</p>
+<button on:click={() => ($countWebSQL -= 1)}>-</button>
+<span>{$countWebSQL}</span>
+<button on:click={() => ($countWebSQL += 1)}>+</button>
+
+<p>indexeddb:</p>
+<button on:click={() => ($countIndexedDB -= 1)}>-</button>
+<span>{$countIndexedDB}</span>
+<button on:click={() => ($countIndexedDB += 1)}>+</button>
